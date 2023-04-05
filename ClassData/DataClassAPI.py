0be739d2c6a5042e1682a229bf85c9d9f3292e59
ChristopherGanzaroli <ganzaroli.christopher@gmail.com>
2023-04-05 23:57:01 +0200

    [DEV] fonction match_query pour entre en paramettre de 'next_pass_url'

--- ClassData/DataClassAPI.py
+++ ClassData/DataClassAPI.py
@@ -10,7 +10,6 @@ class GetDataAPI:
     def __init__(self, url):                # url recupérée lors de l'initialisation de l'objet
         self.url = url
 
-
 ###################################################################################################
 ##   Récupération des données au format json                                                     ##
 ###################################################################################################
@@ -26,22 +25,35 @@ class GetDataAPI:
         array_data = r
         return array_data
 
-
 ###################################################################################################
 ##   Récupération les horaires d'arrivéé                                                         ##
 ##   https://prim.iledefrance-mobilites.fr/fr/donnees-dynamiques/idfm-ivtr-requete_unitaire      ##
-    #ZTckQPr4yBB7roJMCMBbQLS9OKmXbW9P
 ###################################################################################################
 
 
     def next_pass(self):
         r = requests.get(self.url, headers=headers)
             #Affichage du code réponse
-        print('Status:',r)
+        #print('Status:',r)
         #Affichage du contenu de la réponse
-        print(r.content)
+        #print(r.content)
 
-   # url = 'https://prim.iledefrance-mobilites.fr/marketplace/stopmonitoring?MonitoringRef=STIF%3AStopPoint%3AQ%3A22113%3A’
+
+
+###################################################################################################
+##   Récupération d'une clé dans un dictionnaire                                                 ##
+###################################################################################################
+class GetDataFil :
+    def __init__(self, path) :
+        self.path = path
+
+
+    def json_input(self):
+        with open(self.path, "r") as f :
+            return json.loads(f.read())
+    def iter_(self, key):
+        self.json_input()
+        return [i[key] for i in self.json_input().iteritems()]
 
 
 
