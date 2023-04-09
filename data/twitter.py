4b9b5d694015446960f3bcc70ba8f02b5c0f1bc4
ChristopherGanzaroli <ganzaroli.christopher@gmail.com>
2023-04-09 23:52:39 +0200

    [DEV] ajout de la fonction get_tweets

---
+++ data/twitter.py
@@ -0,0 +1,22 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+import datetime
+
+start_date = datetime.datetime.now()
+
+def get_tweets(username):
+    query = f"(from:{username}), since:{start_date:%Y-%m-%d} lang:fr" #until:2023-09-04 since:2023-09-03"
+    tweets = []
+    limit = 3
+    for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+
+        # print(vars(tweet))
+        # break
+        if len(tweets) == limit:
+            break
+        else:
+            tweets.append([tweet.date, tweet.user, tweet.rawContent])
+
+    df = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet'])
+    print(df)
+get_tweets("Ligne2_RATP") #RER_A
