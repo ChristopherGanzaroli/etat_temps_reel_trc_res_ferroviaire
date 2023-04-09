178614201766ab6ebfbdeae74b39fa426991b5ec
ChristopherGanzaroli <ganzaroli.christopher@gmail.com>
2023-04-09 23:52:39 +0200

    [DEV] ajout de la fonction get_tweets

--- shl/start.sh
+++ shl/start.sh
@@ -1,3 +1,5 @@
 #bin/basch
 .\venv\Scripts\activate
-start \elk\logstash-8.6.2\bin\logstash -f \elk\logstash-8.6.2\config\logstash.conf
+source ./venv/Scripts/activate
+
+#start \elk\logstash-8.6.2\bin\logstash -f \elk\logstash-8.6.2\config\logstash.conf
